dbe4616463e3383451efd5c3ccfc611c887b5e86
K-Sieczkowski <sieczkowski.k@northeastern.edu>
2022-12-11 15:19:46 -0500

    DB4O

--- UniversityManagementSystem-Final/src/Business/DB4OUtil/DB4OUtil.java
+++ UniversityManagementSystem-Final/src/Business/DB4OUtil/DB4OUtil.java
@@ -1,22 +1,23 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Business.DB4OUtil;
 
 import Business.ConfigureASystem;
 import Business.EcoSystem;
+import java.nio.file.Paths;
 import com.db4o.Db4oEmbedded;
 import com.db4o.ObjectContainer;
 import com.db4o.ObjectSet;
 import com.db4o.config.EmbeddedConfiguration;
+import com.db4o.query.Predicate;
 import com.db4o.ta.TransparentPersistenceSupport;
-import java.nio.file.Paths;
+
 
 /**
  *
- * @author arfinansari
+ * @author ksiec
  */
 public class DB4OUtil {
     private static final String FILENAME = Paths.get("Databank.db4o").toAbsolutePath().toString();// path to the data store
@@ -39,6 +40,7 @@ public class DB4OUtil {
         try {
 
             EmbeddedConfiguration config = Db4oEmbedded.newConfiguration();
+            ObjectContainer db = Db4oEmbedded.openFile(config, FILENAME);
             config.common().add(new TransparentPersistenceSupport());
             //Controls the number of objects in memory
             config.common().activationDepth(Integer.MAX_VALUE);
@@ -48,21 +50,24 @@ public class DB4OUtil {
             //Register your top most Class here
             config.common().objectClass(EcoSystem.class).cascadeOnUpdate(true); // Change to the object you want to save
 
-            ObjectContainer db = Db4oEmbedded.openFile(config, FILENAME);
             return db;
+            
         } catch (Exception ex) {
             System.out.print(ex.getMessage());
         }
         return null;
     }
+        
 
     public synchronized void storeSystem(EcoSystem system) {
+        
         ObjectContainer conn = createConnection();
         conn.store(system);
         conn.commit();
         conn.close();
     }
     
+
     public EcoSystem retrieveSystem(){
         ObjectContainer conn = createConnection();
         ObjectSet<EcoSystem> systems = conn.query(EcoSystem.class); // Change to the object you want to save
@@ -77,4 +82,15 @@ public class DB4OUtil {
         return system;
     }
     
+    public void deleteOldSystem(ObjectContainer conn) {
+        ObjectSet<EcoSystem> systems = conn.query(new Predicate<EcoSystem>() {
+            @Override
+            public boolean match(EcoSystem et) {
+                return true;
+            }
+        });
+        for (EcoSystem ecoSystem : systems) {
+            conn.delete(ecoSystem);
+        }
+    }
 }
