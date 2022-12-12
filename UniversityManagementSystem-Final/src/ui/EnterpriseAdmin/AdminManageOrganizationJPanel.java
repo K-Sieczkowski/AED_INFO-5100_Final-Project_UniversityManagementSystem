5bfae6f8176128b9c025d88fe7706f461fd6af9a
K-Sieczkowski <sieczkowski.k@northeastern.edu>
2022-12-11 21:47:23 -0500

    admin panel

--- UniversityManagementSystem-Final/src/ui/EnterpriseAdmin/AdminManageOrganizationJPanel.java
+++ UniversityManagementSystem-Final/src/ui/EnterpriseAdmin/AdminManageOrganizationJPanel.java
@@ -59,6 +59,8 @@ public class AdminManageOrganizationJPanel extends javax.swing.JPanel {
         btnAdd = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         btnUpdate = new javax.swing.JButton();
+        orgIDTxt = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
 
         jPanel1.setBackground(new java.awt.Color(204, 255, 204));
         jPanel1.setMinimumSize(new java.awt.Dimension(1050, 1050));
@@ -112,53 +114,72 @@ public class AdminManageOrganizationJPanel extends javax.swing.JPanel {
             }
         });
 
+        orgIDTxt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                orgIDTxtActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setText("Organization ID:");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(lblTitle, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                        .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addGap(237, 237, 237)
-                            .addComponent(lblSelectOrganization, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(347, 347, 347)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(710, 710, 710)
+                        .addComponent(btnUpdate, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(182, 182, 182)
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 651, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 651, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addGroup(jPanel1Layout.createSequentialGroup()
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(lblSelectOrganization, javax.swing.GroupLayout.PREFERRED_SIZE, 236, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                            .addGroup(javax.swing.GroupLayout.Alignment.LEADING, jPanel1Layout.createSequentialGroup()
+                                                .addGap(66, 66, 66)
+                                                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                            .addComponent(orgIDTxt)
+                                            .addComponent(cbOrganization, 0, 270, Short.MAX_VALUE))))
+                                .addGap(84, 84, 84)))))
                 .addContainerGap(217, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(cbOrganization, javax.swing.GroupLayout.PREFERRED_SIZE, 227, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(302, 302, 302))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(btnAdd, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(355, 355, 355))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(55, 55, 55))))
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 136, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(55, 55, 55))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addComponent(lblTitle, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(30, 30, 30)
                 .addComponent(lblTitle, javax.swing.GroupLayout.PREFERRED_SIZE, 58, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(41, 41, 41)
-                .addComponent(jButton1)
-                .addGap(17, 17, 17)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lblSelectOrganization)
-                    .addComponent(cbOrganization, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1)
+                            .addComponent(cbOrganization, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(lblSelectOrganization))
+                        .addGap(18, 18, 18)
+                        .addComponent(orgIDTxt, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jLabel1))
+                .addGap(70, 70, 70)
                 .addComponent(btnAdd)
-                .addGap(28, 28, 28)
+                .addGap(49, 49, 49)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 194, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(30, 30, 30)
                 .addComponent(btnUpdate)
-                .addGap(0, 488, Short.MAX_VALUE))
+                .addGap(0, 463, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -221,7 +242,7 @@ public class AdminManageOrganizationJPanel extends javax.swing.JPanel {
 
     private void btnUpdateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnUpdateActionPerformed
         // TODO add your handling code here:
-   /*      int selectedRowIndex = tblManageOrganization.getSelectedRow();
+      int selectedRowIndex = tblManageOrganization.getSelectedRow();
 
       if (selectedRowIndex < 0) {
             JOptionPane.showMessageDialog(this, "Please select a row to update.");
@@ -230,8 +251,8 @@ public class AdminManageOrganizationJPanel extends javax.swing.JPanel {
 
       else{
            
-          if(cbOrganization.getSelectedItem()){
-              JOptionPane.showMessageDialog(this, "Please fill in all fields.");
+          if(cbOrganization.getSelectedItem().toString().isBlank() || orgIDTxt.getText().equals("")){
+              JOptionPane.showMessageDialog(this, "Please select an organization and fill in the ID.");
             
            }
            
@@ -240,26 +261,29 @@ public class AdminManageOrganizationJPanel extends javax.swing.JPanel {
             DefaultTableModel model = (DefaultTableModel) tblManageOrganization.getModel();
             
             String orgName = cbOrganization.getSelectedItem().toString();
-        
-            Business.Organizations.Organization organization = enterprise.getOrgDirectory().findOrg(orgName);
-            UserAccountDirectory useraccountdirectory = organization.getUserAccountDirectory();
-            Employee e = organization.getEmployeeDirectory().findEmployee(model.getValueAt(selectedRowIndex, 1).toString());
-            UserAccount ua = useraccountdirectory.findUserAccount(model.getValueAt(selectedRowIndex, 1).toString());
-
-
-            ua.getEmployee().setName(cbOrganization.getName());
+            Organization org = enterprise.getOrgDirectory().findOrg(orgName);
             
-            JOptionPane.showMessageDialog(this, "Employee has been updated.");
+            org.setOrgID(Integer.parseInt(orgIDTxt.getText()));
+        
+           
+            JOptionPane.showMessageDialog(this, "The Organization has been updated.");
         }
+          
+          
       }
         cbOrganization.setName("");
+        orgIDTxt.setText("");
         
-        populateTable();     */
+        populateTable();     
     }//GEN-LAST:event_btnUpdateActionPerformed
 
     private void cbOrganizationActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbOrganizationActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_cbOrganizationActionPerformed
+
+    private void orgIDTxtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_orgIDTxtActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_orgIDTxtActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -267,10 +291,12 @@ public class AdminManageOrganizationJPanel extends javax.swing.JPanel {
     private javax.swing.JButton btnUpdate;
     private javax.swing.JComboBox<String> cbOrganization;
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblSelectOrganization;
     private javax.swing.JLabel lblTitle;
+    private javax.swing.JTextField orgIDTxt;
     private javax.swing.JTable tblManageOrganization;
     // End of variables declaration//GEN-END:variables
 
